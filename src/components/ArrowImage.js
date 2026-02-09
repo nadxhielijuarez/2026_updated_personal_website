4dd8ea5031548cab3b351f94f95b28c96f81138b
nadxhielijuarez <nadxhielijuarez@gmail.com>
2026-02-09 17:23:56 -0600

    quick updates before updating css to tailwind

---
+++ src/components/ArrowImage.js
@@ -0,0 +1,7 @@
+import Arrow from '../images/Arrow.png';
+
+export default function ArrowImage() {
+    return <div className="arrow-container"> 
+            <img className="downArrow" src={Arrow} alt="down arrow" />
+        </div>
+}
