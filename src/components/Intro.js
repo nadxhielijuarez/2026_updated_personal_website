b7903f8e832a90f691de1e5aea841932a7c90bd4
nadxhielijuarez <nadxhielijuarez@gmail.com>
2026-04-01 15:30:03 -0500

    done with homepage css

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,28 +1,29 @@
 import PersonalWebsite from '../images/Personal_Website1.png';
 
 export default function Intro() {
-    return  <div className = "Intro-section">
-        <div className="Intro-column">
-            <div id="HeadingContainer" className="vertical-line">
-                <div classname = "HeadingContainText">   
-                    <h1>About</h1><br/>
-                    <h1> Me </h1>                    
+    return <div>
+        <div className = "Intro-section">
+                <div className="Intro-column">
+                    <div id="HeadingContainer" className="vertical-line">
+                        <div classname = "HeadingContainText">   
+                            <h1>About</h1><br/>
+                            <h1> Me </h1>                    
+                        </div>
+                    </div>    
+                    <div className = "intro-row" >I am Software Developer with experience building scalable systems, modernizing codebases, and improving user experiences.<br/> <br/>	
+                </div>          
                 </div>
-            </div>
 
-            <div className = "intro" >I am Software Developer with experience building scalable systems, modernizing codebases, and improving user experiences.<br/> <br/>	
-            </div>
-           
-        </div>
+                <div className="Intro-column">
+                    <div className = "IntroImgContainer">
+                        <img className="IntroImg" src={PersonalWebsite} alt="Personal Website" />
+                    </div>
+                    
+                </div>
+            </div> 
 
-        <div className="Intro-column">
-            <div className = "IntroImgContainer">
-                <img className="IntroImg" src={PersonalWebsite} alt="Personal Website" />
-            </div>
-            
-        </div>
-
-
+                <div className = "intro-row-650" >I am Software Developer with experience building scalable systems, modernizing codebases, and improving user experiences.<br/> <br/>	
+                </div>        
     </div> 
     
 
